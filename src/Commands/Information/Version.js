a8de6d6a45cd3959ba890a25244f07e653be4ed3
ImCactus98 <68060080+ImCactus98@users.noreply.github.com>
2020-07-09 13:56:47 +0200

    Update Version.js

--- src/Commands/Information/Version.js
+++ src/Commands/Information/Version.js
@@ -14,7 +14,8 @@ module.exports = class extends Command {
         verMsg += "This is a bot entirely made in Discord.js and other libraries.\n";
         verMsg += "The Music service is made using Erela.js and Lavalink.\n";
         verMsg += "The bot is made with heart by <@705834500353032234> and <@480987124405895168>\n";
-        verMsg += "__Support Discord server:__ https://discord.gg/bj826WU";
+        verMsg += "__Support Discord server:__ https://discord.gg/bj826WU\n";
+        verMsg += "__GitHub repository:__ https://github.com/ImCactus98/OblivionBot"
         return message.channel.send(verMsg);
     }
 }
